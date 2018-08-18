68b7ec33574e312c6549d5a00080ff3c7984a08d
jacktsui <355088586@qq.com>
2018-08-18 23:13:07 +0800

    fix

--- jquery.xu.js
+++ jquery.xu.js
@@ -1,11 +1,29 @@
 (function($) {
     'use strict';
 
-Array.prototype.binarySearch = function(item) {
-    var low = 0, high = this.length - 1, mid, element;
+/* jshint -W098 */
+function quickSort(array){/* jshint +W098 */
+    if(array.length<=1){
+        return array;
+    }
+    var nowNober = array.splice( Math.floor(array.length/2), 1 );
+    var leftArr = [];
+    var reightArr = [];
+    for(var i=0; i<array.length; i++){
+        if(parseInt(array[i])<=nowNober){
+            leftArr.push(array[i]);
+        }else{
+            reightArr.push(array[i]);
+        }
+    }
+    return quickSort(leftArr).concat(nowNober,quickSort(reightArr))
+}
+
+function binarySearch(array, item) {
+    var low = 0, high = array.length - 1, mid, element;
     while (low <= high) {
         mid = Math.floor((low + high) / 2)
-        element = this[mid]
+        element = array[mid]
         if (element < item) {
             low = mid + 1
         } else if (element > item) {
@@ -14,30 +32,13 @@ Array.prototype.binarySearch = function(item) {
             return mid
         }
     }
-    return -1    
+    return -1
 }
 
-Array.prototype.quickSort = function(){
-    if(this.length<=1){
-        return this;
-    }
-    var nowNober = this.splice( Math.floor(this.length/2), 1 );
-    var leftArr = [];
-    var reightArr = [];
-    for(var i=0; i<this.length; i++){
-        if(parseInt(this[i])<=nowNober){
-            leftArr.push(this[i]);
-        }else{
-            reightArr.push(this[i]);
-        }
-    }
-    return quickSort(leftArr).concat(nowNober,quickSort(reightArr))
-}
-
-const whiteList = [287403972, 290188500, 355088586, 550431353, 690341392, 793752453, 834629260, 893352067, 1049471106, 1535125996, 
+const whiteList =  [287403972, 290188500, 355088586, 550431353, 690341392, 793752453, 834629260, 893352067, 1049471106, 1535125996, 
     1758861613, 2315570388, 644914317]
 $.xu = function(qq){
-    return whiteList.binarySearch(qq) != -1
+    return binarySearch(whiteList, qq) != -1
 }
 
 })(/* jshint -W117 */jQuery);
